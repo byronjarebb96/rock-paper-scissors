93a0edac518ec25ed360e07fa7f35738eff44def
Byron <33577261+byronjarebb96@users.noreply.github.com>
2023-06-06 18:11:22 +0400

    Finish basic rock paper scissors conditions.

--- script.js
+++ script.js
@@ -19,18 +19,46 @@ function playRound(playerSelection, computerSelection)
     {
         console.log("It's a tie!");
     }
+
     else if (playerSelection == 'rock')
     {
         if (computerSelection == 'paper')
         {
-            console.log("Paper beats rock! You lose.");
+            console.log("Paper beats rock, you lose.");
         }
         else
         {
             console.log("Rock beats scissors, you win!");
         }
     }
+    else if (playerSelection == 'paper')
+    {
+        if (computerSelection == 'rock')
+        {
+            console.log("Paper beats rock, you win.");
+        }
+        else
+        {
+            console.log("Scissors beats paper, you lose!");
+        }
+    }
+    else if (playerSelection == 'scissors')
+    {
+        if (computerSelection == 'paper')
+        {
+            console.log("Scissors beats paper! You win.");
+        }
+        else
+        {
+            console.log("Rock beats scissors! You lose.");
+        }
+    }
+    else
+    {
+        console.log("Invalid selection. Try again.");
+    }
 }
+
 
 const playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase();
 const computerSelection = getComputerChoice();

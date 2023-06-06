f4b751fb95fdf6daade1f4f761075ac2ba4e6984
Byron <33577261+byronjarebb96@users.noreply.github.com>
2023-06-06 14:02:20 +0400

    Add tie condition, rock playerchoice

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function getComputerChoice(){
+function getComputerChoice()
+{
     randomInteger=Math.round(Math.random()*(3-1)+1);
     switch(randomInteger)
     {
@@ -10,3 +11,27 @@ function getComputerChoice(){
             return("scissors");
     }
 }
+
+function playRound(playerSelection, computerSelection)
+{
+    console.log(`Player chose ${playerSelection}, computer chose ${computerSelection}`)
+    if (playerSelection === computerSelection)
+    {
+        console.log("It's a tie!");
+    }
+    else if (playerSelection == 'rock')
+    {
+        if (computerSelection == 'paper')
+        {
+            console.log("Paper beats rock! You lose.");
+        }
+        else
+        {
+            console.log("Rock beats scissors, you win!");
+        }
+    }
+}
+
+const playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase();
+const computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);

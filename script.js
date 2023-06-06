57c12198936f624d00e161d3aa45a0f78b83476b
Byron <33577261+byronjarebb96@users.noreply.github.com>
2023-06-06 18:35:08 +0400

    Add basic game functionality

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ function getComputerChoice()
     }
 }
 
-function playRound(playerSelection, computerSelection)
+function playRound(/*playerSelection, computerSelection*/)
 {
+    const playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase();
+    const computerSelection = getComputerChoice();
+
     console.log(`Player chose ${playerSelection}, computer chose ${computerSelection}`)
     if (playerSelection === computerSelection)
     {
@@ -25,10 +28,12 @@ function playRound(playerSelection, computerSelection)
         if (computerSelection == 'paper')
         {
             console.log("Paper beats rock, you lose.");
+            return computerScore += 1;
         }
         else
         {
             console.log("Rock beats scissors, you win!");
+            return playerScore += 1;
         }
     }
     else if (playerSelection == 'paper')
@@ -36,10 +41,12 @@ function playRound(playerSelection, computerSelection)
         if (computerSelection == 'rock')
         {
             console.log("Paper beats rock, you win.");
+            return playerScore += 1;
         }
         else
         {
             console.log("Scissors beats paper, you lose!");
+            return computerScore += 1;
         }
     }
     else if (playerSelection == 'scissors')
@@ -47,10 +54,12 @@ function playRound(playerSelection, computerSelection)
         if (computerSelection == 'paper')
         {
             console.log("Scissors beats paper! You win.");
+            return playerScore += 1;
         }
         else
         {
             console.log("Rock beats scissors! You lose.");
+            return computerScore += 1;
         }
     }
     else
@@ -59,7 +68,35 @@ function playRound(playerSelection, computerSelection)
     }
 }
 
+function game(rounds)
+{
+    for (i=0; i<rounds; i++)
+    {
+        playRound();
+        console.log(`Player: ${playerScore} | Computer: ${computerScore}`)
+    }
+}
+let playerScore = 0;
+let computerScore = 0;
+rounds = prompt("How many rounds? ");
+game(rounds);
+if (playerScore > computerScore)
+{
+    console.log("======\nYou win.\n======");
+}
+else if (playerScore < computerScore)
+{
+    console.log("======\nYou lose.\n======");
+}
+else
+{
+    console.log("======\nIt's a tie.\n======");
+}
 
+/*
 const playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase();
 const computerSelection = getComputerChoice();
 playRound(playerSelection, computerSelection);
+*/
+
+

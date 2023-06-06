4ade9a09930c8c27ac6e4fff89d6d6a41d91ce57
Byron <33577261+byronjarebb96@users.noreply.github.com>
2023-06-06 13:52:17 +0400

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!")
+function getComputerChoice(){
+    randomInteger=Math.round(Math.random()*(3-1)+1);
+    switch(randomInteger)
+    {
+        case 1:
+            return("rock");
+        case 2:
+            return ("paper");
+        case 3:
+            return("scissors");
+    }
+}
